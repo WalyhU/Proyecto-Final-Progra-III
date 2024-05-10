836cf0a0c97568e699a4d67a141f11f0e52df719
Waly <38843113+WalyhU@users.noreply.github.com>
2024-05-09 23:35:02 -0600

    Inicialización de Proyecto

---
+++ api-negocios/src/test/java/com/example/apinegocios/ApiNegociosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apinegocios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNegociosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

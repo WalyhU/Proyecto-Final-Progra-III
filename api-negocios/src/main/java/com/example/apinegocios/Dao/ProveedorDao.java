a372d044cbb19780530c6c7cdd982200d4e87c4e
Marta <85373964+Hinata1411@users.noreply.github.com>
2024-05-11 00:14:15 -0600

    Creación de Dao ProveedorDao

---
+++ api-negocios/src/main/java/com/example/apinegocios/Dao/ProveedorDao.java
@@ -0,0 +1,7 @@
+package com.example.apinegocios.Dao;
+
+import com.example.apinegocios.Entities.Proveedor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProveedorDao extends CrudRepository<Proveedor, Long> {
+}

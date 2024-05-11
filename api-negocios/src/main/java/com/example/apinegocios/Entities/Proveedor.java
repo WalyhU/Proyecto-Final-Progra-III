8e2e67b97486144669ad1a1aad2488f5bed4598c
Marta <85373964+Hinata1411@users.noreply.github.com>
2024-05-11 00:03:09 -0600

    Creación de nueva rama y entidad Proveedor

---
+++ api-negocios/src/main/java/com/example/apinegocios/Entities/Proveedor.java
@@ -0,0 +1,82 @@
+package com.example.apinegocios.Entities;
+
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "proveedor")
+public class Proveedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String direction;
+    private String telefono;
+    private String producto;
+    private String email;
+    @Column(name = "created_At")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+
+
+}
